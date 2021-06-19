4328c5570d0d043b0034d7341190054bc1476b82
snpark <snpark@student.42seoul.kr>
2021-06-19 10:26:45 +0900

    char to binary

--- client.c
+++ client.c
@@ -6,28 +6,33 @@
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 14:27:20 by snpark            #+#    #+#             */
-/*   Updated: 2021/06/18 16:54:11 by snpark           ###   ########.fr       */
+/*   Updated: 2021/06/19 10:25:59 by snpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <stdio.h>
 
 int		send(int pid, char *message)
 {
-	int i;
+	int		i;
+	char	c;
 
-	i = 0;
-	while (message)
+	while (*message)
 	{
-		kill(pid);
-		*message >>= 1;
-		i++;
-		if (i == 8)
+		c = *message;
+		i = 0;
+		while (i < 8)
 		{
-			i = 0;
-			message++;
+			if ((c >> (7 - i)) % 2 == 1)
+				write(1, "1", 1);
+			else
+				write(1, "0", 1);
+			i++;
 		}
+		message++;
 	}
+	return (1);
 }
 
 int		main(int arg_i, char **arg_s)
@@ -37,8 +42,7 @@ int		main(int arg_i, char **arg_s)
 	if (arg_i == 3)
 	{
 		pid = ft_atoi(arg_s[1]);
-		ft_putnbr(pid);
-		write(1, arg_s[2], ft_strlen(arg_s[2]));
+		send(pid, arg_s[2]);
 	}
 	return (0);
 }
