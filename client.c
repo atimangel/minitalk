0d611319e1ca61789328034079173fe76f17bc42
snpark <snpark@student.42seoul.kr>
2021-06-19 17:11:38 +0900

    commit&push

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 14:27:20 by snpark            #+#    #+#             */
-/*   Updated: 2021/06/19 10:25:59 by snpark           ###   ########.fr       */
+/*   Updated: 2021/06/19 16:11:19 by snpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,15 @@ int		send(int pid, char *message)
 		while (i < 8)
 		{
 			if ((c >> (7 - i)) % 2 == 1)
+			{
+				kill(pid, SIGUSR1);
 				write(1, "1", 1);
+			}
 			else
+			{
+				kill(pid, SIGUSR2);
 				write(1, "0", 1);
+			}
 			i++;
 		}
 		message++;
@@ -42,6 +48,11 @@ int		main(int arg_i, char **arg_s)
 	if (arg_i == 3)
 	{
 		pid = ft_atoi(arg_s[1]);
+		if (kill(pid, 0) == -1)
+		{
+			write(1, "Error\n", 6);
+			exit();
+		}
 		send(pid, arg_s[2]);
 	}
 	return (0);
--- server.c
+++ server.c
@@ -6,17 +6,23 @@
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 14:27:02 by snpark            #+#    #+#             */
-/*   Updated: 2021/06/16 14:13:20 by snpark           ###   ########.fr       */
+/*   Updated: 2021/06/19 14:45:48 by snpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h>
 
+
+
 int		main(void)
 {
-	pid_t pid;
+	pid_t		pid;
+	sigset_t	sigset;
 
+	sigempty(&sigset);
+	sigaddset(&sigset, SIGUSR1);
+	sigaddset(&sigset, SIGUSR2);
 	pid = getpid();
 	ft_putnbr(pid);
 	//print 
