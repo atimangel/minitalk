b104c6450bd15dff6cc4e0cb55fe88fcfc2dcee9
aaa <atimangel@naver.com>
2021-06-22 19:41:22 +0900

    why use sigactio? so use signal

--- server.c
+++ server.c
@@ -6,39 +6,34 @@
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 14:27:02 by snpark            #+#    #+#             */
-/*   Updated: 2021/06/22 19:29:11 by senoi            ###   ########.fr       */
+/*   Updated: 2021/06/22 19:40:44 by senoi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
 
-static char c;
-static int 	i;
+static char_buf_t buf
 
 void	handle_signal(int sig)
 {
-	c = c << 1;
-	i++;
+	buf.c = buf.c << 1;
+	buf.i++;
 	if (sig == SIGUSR2)
-		c++;
+		buf.c++;
 	if (i == 8)
 	{
-		write(1, &c, 1);
-		c = 0;
-		i = 0;
+		write(1, &buf.c, 1);
+		buf.c = 0;
+		buf.i = 0;
 	}
 }
 
 int		main(void)
 {
 	pid_t		pid;
-	struct sigaction act;
 
-	act.sa_flags = SA_NODEFER;
-	act.sa_handler = handle_signal;
-	sigaction(SIGUSR1, &act, 0);
-	sigaction(SIGUSR2, &act, 0);
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
 	pid = getpid();
 	ft_putnbr(pid);
 	write(1, "\n", 1);
