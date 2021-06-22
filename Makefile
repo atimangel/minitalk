20cd585c9fbfbbfec7a84ebc7b625f23ff78f937
aaa <atimangel@naver.com>
2021-06-22 15:43:44 +0900

    comunicate 01 with client and server

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/16 13:44:48 by snpark            #+#    #+#              #
-#    Updated: 2021/06/18 16:46:11 by snpark           ###   ########.fr        #
+#    Updated: 2021/06/22 11:56:56 by senoi            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,19 +14,19 @@ NAME = server
 
 all: $(NAME) client
 
-$(NAME):
-	make -C libft
+$(NAME): libft.a
 	gcc -o server.out server.c utils.c -l ft -L libft -I libft
 
-client:
-	make -C libft
+client: libft.a
 	gcc -o client.out client.c utils.c -l ft -L libft -I libft
 
 clean:
 	rm libft/*.o
 
+libft.a:
+	make -C libft
+
 fclean: clean
-	rm libft/*.a
-	rm *.out
+	rm libft/*.a *.out
 
 re: fclean all client
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 14:27:02 by snpark            #+#    #+#             */
-/*   Updated: 2021/06/19 14:45:48 by snpark           ###   ########.fr       */
+/*   Updated: 2021/06/22 15:40:16 by senoi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,51 @@
 #include <stdio.h>
 
 
+void	test(int sig)
+{
+	if (sig == SIGUSR1)
+		write(1, "0", 1);
+	if (sig == SIGUSR2)
+		write(1, "1", 1);
+}
 
 int		main(void)
 {
 	pid_t		pid;
 	sigset_t	sigset;
+	void	(*ft)(int);
+	struct sigaction	arg_sa;
+	struct sigaction	tmp;
 
-	sigempty(&sigset);
-	sigaddset(&sigset, SIGUSR1);
-	sigaddset(&sigset, SIGUSR2);
+	arg_sa.sa_flags = SA_NODEFER;
+	arg_sa.sa_handler = test;
+//signal setting
+	sigemptyset(&sigset);
+	if (!sigaddset(&sigset, SIGUSR1))
+		printf("ok 1\n");
+	else
+		printf("fail 1\n");
+	if (!sigaddset(&sigset, SIGUSR2))
+		printf("ok 2\n");
+	else
+		printf("fail 2\n");
+	if (sigismember(&sigset, SIGUSR1))
+		printf("in it 1\n");
+	else
+		printf("no 1\n");
+	if (sigismember(&sigset, SIGUSR2))
+		printf("in it 2\n");
+	else
+		printf("no 2\n");
+	arg_sa.sa_mask = sigset;
+	signal(SIGUSR1, test);
+	signal(SIGUSR2, test);
+//get pid & print pid
 	pid = getpid();
 	ft_putnbr(pid);
-	//print 
+	write(1, "\n", 1);
+	while (1)
+		pause();
+	write(1, "\n", 1);
+	printf("end\n");
 }
